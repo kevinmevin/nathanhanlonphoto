1b87e6f8b997f8ead96b2760dc7b08c46fbfedeb
Kevin Grissom <kevinmevin@gmail.com>
2013-08-29 12:14:11 -0700

    rails with foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nathanhanlonphoto::Application.config.secret_token = '418e61acdb4bacef55fd7165246629c70f4a21a3c16ff328423df72bbfb8f6636571368df6f131e5898f2334ebc6ab7ad04cbcba59c68b048e513f2d831e0d83'
